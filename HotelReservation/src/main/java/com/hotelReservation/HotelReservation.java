80a4f8d6159a1db6f1bc13d3e263b5b2e5966d07
Vivek varshney <Vivek.iitd93@gmail.com>
2021-08-04 08:37:29 +0530

    Display: Initial comment

---
+++ HotelReservation/src/main/java/com/hotelReservation/HotelReservation.java
@@ -0,0 +1,15 @@
+package com.hotelReservation;
+/**
+ * <h1>Hotel Reservation Program</h1>
+ * <p>Displaying Welcome to Hotel Reservation Program</p>
+ *
+ * @author vivek kumar
+ * @version 1.0
+ * @since 2021-08-04
+ */
+public class HotelReservation {
+    public static void main(String[] args) {
+        // Displaying Welcome to Hotel Reservation Program
+        System.out.println("Welcome to hotel reservation program !!");
+    }
+}
